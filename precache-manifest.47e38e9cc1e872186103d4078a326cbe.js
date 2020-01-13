66b0d4ea0d3818e4277912542d1e0f388121e429
Дмитрий Выприченко <d.vyprichenko@avto.pro>
2020-01-14 00:34:38 +0200

    Updates

---
+++ precache-manifest.47e38e9cc1e872186103d4078a326cbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e08248dcc272aeddb7e843f956180fa1",
+    "url": "/vin-decoder/index.html"
+  },
+  {
+    "revision": "8a61b370eb7dee2e033c",
+    "url": "/vin-decoder/static/css/main.f26f4156.chunk.css"
+  },
+  {
+    "revision": "9820f44a2edea79d19b7",
+    "url": "/vin-decoder/static/js/2.856566ce.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/vin-decoder/static/js/2.856566ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8a61b370eb7dee2e033c",
+    "url": "/vin-decoder/static/js/main.84938c7a.chunk.js"
+  },
+  {
+    "revision": "72d76fadfb0e10e0cedd",
+    "url": "/vin-decoder/static/js/runtime-main.50e0ba54.js"
+  }
+]);
